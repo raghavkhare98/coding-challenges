cf16070ef3ab37aed891af5462ec6a31d6e76082
raghavkhare98 <khare08rma@gmail.com>
2025-01-02 21:14:47 -0500

    Write code for c flag which returns file size

--- challenge-wctool/wcTool.py
+++ challenge-wctool/wcTool.py
@@ -1,7 +1,20 @@
 import os
+import sys
+import argparse
 
-fileName = "test.txt"
+class CAction(argparse.Action):
 
-fileStats = os.stat(fileName)
+    def __call__(self, parser, namespace, values, option_string=None):
+        
+        if os.path.exists(values[0]):
+            fileStats = os.stat(values[0])
+            print(f'{fileStats.st_size}', end=" ")
+            print(f'{values[0]}')
+            # setattr(namespace, self.dest, fileStats.st_size, values[0])
+        else:
+            print("Please provide the correct file path")
+            
+parser = argparse.ArgumentParser() #this will help us in commandline flags
 
-print(f'File size in Bytes: {fileStats.st_size}')
+parser.add_argument('-c', action=CAction, nargs=1)
+args = parser.parse_args(["-c", sys.argv[2]])
