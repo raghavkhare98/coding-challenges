96cef18c71b1efe1bd8cbba409225fc643963e3a
raghavkhare98 <khare08rma@gmail.com>
2025-01-03 10:46:54 -0500

    Complete code for w flag in the wctool challenge

--- challenge-wctool/wcTool.py
+++ challenge-wctool/wcTool.py
@@ -39,17 +39,43 @@ def lFlag(fileName):
             else:
                 return "File Not found. Please provide the correct file path"
         
-    parser = argparse.ArgumentParser() #this will help us in commandline flags
+    parser = argparse.ArgumentParser()
 
     parser.add_argument('-l', action=LAction, nargs=1)
-    args = parser.parse_args(["-l", sys.argv[2]])
+    parser.parse_args(["-l", sys.argv[2]])
 
+def wFlag(fileName):
     
+    class WAction(argparse.Action):
+
+        def __call__(self, parser, namespace, values, option_string=None):
+            
+            if os.path.exists(fileName):
+        
+                wordCount = 0
+                wordList = []
+                with open(f'{fileName}', 'r') as f:
+                    for i in f:
+                        wordList.append(i.split())
+                for i in wordList:
+                    wordCount += len(i)
+                print(wordCount, end=" ")
+                print(f'{values[0]}') 
+            
+            else:
+                return "File Not found. Please provide the correct file path"
+        
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('-w', action=WAction, nargs=1)
+    parser.parse_args(["-w", sys.argv[2]])    
 
 if sys.argv[1] == '-c':
     cFlag(sys.argv[2])
-else:
+elif sys.argv[1] == "-l":
     lFlag(sys.argv[2])
+elif sys.argv[1] == "-w":
+    wFlag(sys.argv[2])
 
     
 
