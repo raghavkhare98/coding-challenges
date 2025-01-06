82ecd53659d395bc82027144d36bf8c20711f923
raghavkhare98 <khare08rma@gmail.com>
2025-01-06 16:10:40 -0500

    Complete code for multiple inputs and cat

--- challenge-wctool/wcTool.py
+++ challenge-wctool/wcTool.py
@@ -2,114 +2,138 @@ import os
 import sys
 import argparse
 
-def cFlag(fileName):
-    
-    class CAction(argparse.Action):
-
-        def __call__(self, parser, namespace, values, option_string=None):
+def cFlag(fileName=None):
             
-            if os.path.exists(values[0]):
-                fileStats = os.stat(values[0])
-                print(f'{fileStats.st_size}', end=" ")
+    try:
+        if fileName:
+            
+            fileStats = os.stat(fileName)
+            print(f'{fileStats.st_size}', end=" ")
+            
+        else:
+            data = sys.stdin.read()
+            lineCount = data.count("\n")
+            print(f'{lineCount}')
+            
+    except Exception as e:
+        
+        print("Please provide the correct file path")
+
+    
+
+def lFlag(fileName=None):
+    
+    try:        
+        count = 0
+
+        if fileName:
+            
+            with open(f'{fileName}', "rb") as f:
+                for i, _ in enumerate(f):
+                    count += 1
+            print(count, end=" ")
+
+        else:
+
+            data = sys.stdin.read()
+            for i, _ in enumerate(data):
+                count += 1
+            print(count)
+            
+            
+    
+    except Exception as e:
+        
+        return "File Not found. Please provide the correct file path"
+
+def wFlag(fileName=None):
+    
+            
+    try:
+        
+        if fileName:
+
+            with open(f'{fileName}', 'r') as f:
+                for i in f:
+                    wordCount = len(i.split())
+            print(wordCount, end=" ")
+            
+
+        else:
+
+            data = sys.stdin.read()
+            wordCount = len(data.split())
+            print(wordCount) 
+    
+    except Exception as e:
+        
+        return "File Not found. Please provide the correct file path"
+
+def mFlag(fileName=None):
+
+    charCount = 0
+
+    try:
+        
+        if fileName:
+            
+            with open(f'{fileName}', 'r', encoding="UTF-8") as f:
+                for i in f:
+                    charCount += len(i)
+
+            print(charCount, end=" ")
+
+        else:
+            
+            data = sys.stdin.read()
+            for i in data:
+                charCount += len(i)
+            
+            print(charCount, end=" ")
                 
-                # setattr(namespace, self.dest, fileStats.st_size, values[0])
-            else:
-                print("Please provide the correct file path")
-                
-    parser = argparse.ArgumentParser() #this will help us in commandline flags
+    except Exception as e:
+        return "File Not found. Please provide the correct file path"
 
-    parser.add_argument('-c', action=CAction, nargs=1)
-    args = parser.parse_args(["-c", f'{fileName}'])
 
-def lFlag(fileName):
-    
-    class LAction(argparse.Action):
+def main():
 
-        def __call__(self, parser, namespace, values, option_string=None):
-            
-            if os.path.exists(fileName):
-        
-                count = 0
-                with open(f'{fileName}', "rb") as f:
-                    for i, _ in enumerate(f):
-                        count += 1
-                print(count, end=" ")
-                 
-            
-            else:
-                return "File Not found. Please provide the correct file path"
-        
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', action="store_true")
+    parser.add_argument('-l', action="store_true")
+    parser.add_argument('-w', action="store_true")
+    parser.add_argument('-m', action="store_true")
 
-    parser.add_argument('-l', action=LAction, nargs=1)
-    parser.parse_args(["-l", f'{fileName}'])
+    parser.add_argument("files", nargs="*")
 
-def wFlag(fileName):
-    
-    class WAction(argparse.Action):
+    args = parser.parse_args()
 
-        def __call__(self, parser, namespace, values, option_string=None):
-            
-            if os.path.exists(fileName):
+    if not(args.c or args.l or args.w or args.m):
+
+        args.m = args.l = args.c = True
         
-                wordCount = 0
-                wordList = []
-                with open(f'{fileName}', 'r') as f:
-                    for i in f:
-                        wordList.append(i.split())
-                for i in wordList:
-                    wordCount += len(i)
-                print(wordCount, end=" ") 
-            
-            else:
-                return "File Not found. Please provide the correct file path"
+
+    if not args.files:
         
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('-w', action=WAction, nargs=1)
-    parser.parse_args(["-w", f'{fileName}']) 
-
-def mFlag(fileName):
-
-    class MAction(argparse.Action):
-
-        def __call__(self, parser, namespace, values, option_string = None):
-            
-            if os.path.exists(f'{fileName}'):
-                charCount = 0
-                with open(f'{fileName}', 'r', encoding="UTF-8") as f:
-                    for i in f:
-                        charCount += len(i)
-
-                print(charCount, end=" ")
-                
-
-            else:
-                return "File Not found. Please provide the correct file path"
+        fileName = None
     
-    parser = argparse.ArgumentParser()
+    else:
 
-    parser.add_argument("-m", action=MAction)
-    parser.parse_args(["-m", f'{fileName}'])
+        fileName = args.files
 
-if len(sys.argv) > 2 and sys.argv[1] == '-c':
-    cFlag(sys.argv[2])
-    print(f'{sys.argv[2]}')
-elif len(sys.argv) > 2 and sys.argv[1] == "-l":
-    lFlag(sys.argv[2])
-    print(f'{sys.argv[2]}')
-elif len(sys.argv) > 2 and sys.argv[1] == "-w":
-    wFlag(sys.argv[2])
-    print(f'{sys.argv[2]}')
-elif len(sys.argv) > 2 and sys.argv[1] == "-m":
-    mFlag(sys.argv[2])
-    print(f'{sys.argv[2]}')
-elif len(sys.argv) <= 2:
-    lFlag(sys.argv[1])
-    wFlag(sys.argv[1])
-    cFlag(sys.argv[1])
-    print(f'{sys.argv[1]}')
-    
+    for file in fileName:
 
-    
+        if args.c:
+            cFlag(file)
+            print(file)
+        if args.l:
+            lFlag(file)
+            print(file)
+        if args.m:
+            mFlag(file)
+            print(file)
+        if args.w:
+            wFlag(file)
+            print(file)
 
+if __name__ == "__main__":
+    main()
