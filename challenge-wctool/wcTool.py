16b0bc14a665d2c1380222f302174541e4548d37
raghavkhare98 <khare08rma@gmail.com>
2025-01-06 13:25:37 -0500

    Write code for no flag in command line

--- challenge-wctool/wcTool.py
+++ challenge-wctool/wcTool.py
@@ -11,7 +11,7 @@ def cFlag(fileName):
             if os.path.exists(values[0]):
                 fileStats = os.stat(values[0])
                 print(f'{fileStats.st_size}', end=" ")
-                print(f'{values[0]}')
+                
                 # setattr(namespace, self.dest, fileStats.st_size, values[0])
             else:
                 print("Please provide the correct file path")
@@ -19,7 +19,7 @@ def cFlag(fileName):
     parser = argparse.ArgumentParser() #this will help us in commandline flags
 
     parser.add_argument('-c', action=CAction, nargs=1)
-    args = parser.parse_args(["-c", sys.argv[2]])
+    args = parser.parse_args(["-c", f'{fileName}'])
 
 def lFlag(fileName):
     
@@ -34,7 +34,7 @@ def lFlag(fileName):
                     for i, _ in enumerate(f):
                         count += 1
                 print(count, end=" ")
-                print(f'{values[0]}') 
+                 
             
             else:
                 return "File Not found. Please provide the correct file path"
@@ -42,7 +42,7 @@ def lFlag(fileName):
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-l', action=LAction, nargs=1)
-    parser.parse_args(["-l", sys.argv[2]])
+    parser.parse_args(["-l", f'{fileName}'])
 
 def wFlag(fileName):
     
@@ -59,8 +59,7 @@ def wFlag(fileName):
                         wordList.append(i.split())
                 for i in wordList:
                     wordCount += len(i)
-                print(wordCount, end=" ")
-                print(f'{values[0]}') 
+                print(wordCount, end=" ") 
             
             else:
                 return "File Not found. Please provide the correct file path"
@@ -68,14 +67,49 @@ def wFlag(fileName):
     parser = argparse.ArgumentParser()
 
     parser.add_argument('-w', action=WAction, nargs=1)
-    parser.parse_args(["-w", sys.argv[2]])    
+    parser.parse_args(["-w", f'{fileName}']) 
 
-if sys.argv[1] == '-c':
+def mFlag(fileName):
+
+    class MAction(argparse.Action):
+
+        def __call__(self, parser, namespace, values, option_string = None):
+            
+            if os.path.exists(f'{fileName}'):
+                charCount = 0
+                with open(f'{fileName}', 'r', encoding="UTF-8") as f:
+                    for i in f:
+                        charCount += len(i)
+
+                print(charCount, end=" ")
+                
+
+            else:
+                return "File Not found. Please provide the correct file path"
+    
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-m", action=MAction)
+    parser.parse_args(["-m", f'{fileName}'])
+
+if len(sys.argv) > 2 and sys.argv[1] == '-c':
     cFlag(sys.argv[2])
-elif sys.argv[1] == "-l":
+    print(f'{sys.argv[2]}')
+elif len(sys.argv) > 2 and sys.argv[1] == "-l":
     lFlag(sys.argv[2])
-elif sys.argv[1] == "-w":
+    print(f'{sys.argv[2]}')
+elif len(sys.argv) > 2 and sys.argv[1] == "-w":
     wFlag(sys.argv[2])
+    print(f'{sys.argv[2]}')
+elif len(sys.argv) > 2 and sys.argv[1] == "-m":
+    mFlag(sys.argv[2])
+    print(f'{sys.argv[2]}')
+elif len(sys.argv) <= 2:
+    lFlag(sys.argv[1])
+    wFlag(sys.argv[1])
+    cFlag(sys.argv[1])
+    print(f'{sys.argv[1]}')
+    
 
     
 
